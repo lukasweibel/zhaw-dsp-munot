aceeabf646bc83469606aa5b40c5fb6390d8f652
Lukas Weibel <lukasweibel@Lukass-MacBook-Air.local>
2024-10-16 22:54:51 +0200

    Add some sort of history and give the result back in natural language

--- backend/layers/text_layer.py
+++ backend/layers/text_layer.py
@@ -1,2 +1,10 @@
-def result_set_to_natural_language(resultset):
-    return "HELLO TEXT"
+from backend.common.file_helper import load_txt_file
+from backend.common.llm_request import generate_text
+
+
+def result_set_to_natural_language(original_question, resultset):
+    base_prompt = load_txt_file('./backend/layers/text_layer_prompt.txt')
+    prompt = base_prompt.format(
+        user_frage=original_question, resultset=resultset)
+    answer = generate_text(prompt)
+    return answer
