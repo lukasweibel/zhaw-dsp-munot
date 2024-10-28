e6bad566dc5f5523f8c580dc2b8c350194848d3e
nadirfl <nadir-ferlin@hotmail.com>
2024-10-28 20:32:53 +0100

    add tests

--- test/IntegrationTests.py
+++ test/IntegrationTests.py
@@ -13,3 +13,42 @@ class IntegrationTest(unittest.TestCase):
         actual = ask_question("Welcher Verein war erster im Boden turnen?")
         a = assert_meaning("Keine Daten vorhanden", actual)
         self.assertTrue(a)
+        
+    def test_gruendungsjahr_TV_Egg(self):
+        actual = ask_question("Was ist das Grundüngsjahr der TV Egg?")
+        a = assert_meaning("Das Gründungsjahr der TV Egg ist 1902", actual)
+        self.assertTrue(a)
+    
+    def test_rang_und_disziplin_russikon(self):
+        # TODO: expected herausfinden
+        actual = ask_question("Auf welchem Rang war Russikon? Und in welcher Disziplin?")
+        a = assert_meaning("Russikon war auf dem 6. Rang in der Disziplin XY", actual)
+        self.assertTrue(a)
+
+    def test_rang_russikon_team_aerobic_und_maximalpunktzahl(self):
+        # TODO: expected herausfinden
+        actual = ask_question("Auf welchem Rang war Russikon in der Disziplin Team Aerobic und was ist die maximale Punktzahl?")
+        a = assert_meaning("Russikon war auf dem ersten Rang in der Disziplin Team Aerobic. Die maximale Punktzahl beträgt X.", actual)
+        self.assertTrue(a)
+    
+    def test_zeit_TV_Egg_BA(self):
+        # TODO: expected herausfinden
+        actual = ask_question("Wann turnt der TV Egg BA?")
+        a = assert_meaning("Der Verein TV Egg BA turnt um XY", actual)
+        self.assertTrue(a)
+        
+    def test_abzug_verstoss(self):
+        actual = ask_question("Wieviel Abzug gibt Verstoss gegen die Teilnahmeberechtigung?")
+        a = assert_meaning("Ein Verstoss gegen die Teilnahmeberechtigung führt zu einem Abzug von 1.0 Punkt.", actual)
+        self.assertTrue(a)
+    
+    def test_zeit_TSV_Rohrdorf_Kategorie_RE(self):
+        # TODO: expected herausfinden
+        actual = ask_question("Wann turnt der TSV Rohrdorf in der Kategorie RE?")
+        a = assert_meaning("Der Verein TSV Rohrdorf turnt um XY in der Kategorie RE", actual)
+        self.assertTrue(a)
+        
+    def test_disziplin_TV_Andelfingen_Zeit(self):
+        actual = ask_question("Welche Disziplin turnt der Verein TV Andelfingen um 13:40?")
+        a = assert_meaning("Der Verein TV Andelfingen turn um 13:40 die Disziplin BA", actual)
+        self.assertTrue(a)
