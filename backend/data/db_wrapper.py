f71cdfac0dfc073ac9565e8fee25b743d3a1deab
nadirfl <nadir-ferlin@hotmail.com>
2025-04-21 18:27:40 +0200

    add call to API and SQL validation after generation

--- backend/data/db_wrapper.py
+++ backend/data/db_wrapper.py
@@ -1,4 +1,23 @@
 from backend.data import in_memory_db
+import requests
+import json
 
 def execute_sql(sql_string):
     return in_memory_db.execute_sql(sql_string)
+
+def execute_sql_via_api(sql):
+    url = "https://rangliste.munotcup.ch/backend/raw/878c5b1e-fe76-400e-92f4-2d263c05ce40"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "query": sql
+    }
+    
+    try:
+        response = requests.post(url, headers=headers, json=payload)
+        response.raise_for_status()
+        return response.json
+    except requests.exceptions.RequestException as e:
+        print("API returned an error")
+        return {"result": "Fehler bei der Datenbankabfrage"}
